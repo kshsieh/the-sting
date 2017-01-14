3f13639203cbee9236d09b75dc996750d8b35fdb
Kevin Hsieh <kshsieh@wework.com>
2017-01-14 10:07:14 -0800

    fix count plural bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
   def effort_to_string(int)
     "< #{int.ceil_to(5)} minutes"
   end
+
+  def issues_count_to_string(int)
+    string = int > 1 ? "current issues" : "current issue"
+    "#{int} #{string}"
+  end
 end
