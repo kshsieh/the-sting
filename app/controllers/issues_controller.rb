a02dede2bc579f3fb1692fbbaad51a7ae73602a2
Kevin Hsieh <kshsieh@wework.com>
2017-01-14 19:45:26 -0800

    cta button

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,13 +1,20 @@
 class IssuesController < ApplicationController
   before_action :load_issue
+  before_action :set_title
 
   def show
-    @title = @issue.topic.name
+  end
+
+  def script
   end
 
   private
 
   def load_issue
     @issue = Issue.find(params[:id])
+  end
+
+  def set_title
+    @title = @issue.topic.name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :issues, only: [:show]
+  resources :issues, only: [:show] do
+    member do
+      get :script
+    end
+  end
   
   resources :topics, only: [:index, :show] do
     resources :issues, only: [:index]
