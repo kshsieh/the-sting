b322822c9bff34b0f1bc3bab321646be0edae188
Kevin Hsieh <kshsieh@wework.com>
2017-01-16 11:16:18 -0800

    topic/issues index

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,17 +1,23 @@
 class IssuesController < ApplicationController
-  before_action :load_issue
-  before_action :set_title
+  before_action :load_issue, only: [:show]
+  before_action :load_topic, only: [:index]
 
   def show
+  end
+
+  def index
+
   end
 
   private
 
   def load_issue
     @issue = Issue.find(params[:id])
+    @title = @issue.topic.name
   end
 
-  def set_title
-    @title = @issue.topic.name
+  def load_topic
+    @topic = Topic.find(params[:topic_id])
+    @title = @topic.name
   end
 end
